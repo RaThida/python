72726b8fe8f9f423d6ee041137bae8f162c55ee8
RaThida <100819767+RaThida@users.noreply.github.com>
2022-07-14 10:01:47 +0700

    Add files via upload

---
+++ array[].py
@@ -0,0 +1,59 @@
+# 9. given an aray[] of n elements, write a python fucntion to search a given element.
+# from itertools import islice
+# test_list = [2, 4, 5, 8, 12, 42, 6, 10]
+# print("The orgin list:" +str(test_list))
+# n = 12
+# res = list(islice(reversed(test_list), 0, n))
+# res.reverse()
+# print("the last n element of the list are: " +str(res))
+
+# import array as arr
+# b = arr.array('i', [2, 3, 4])
+# print("aray before insertion: ", end = " ")
+# for i in range (0, 1):
+#     print(b[i], end = " ")
+# print()
+
+# b.insert(1, 3)
+# print("array after insertion: ", end = " ")
+# for i in (b):
+#     print(i, end = " ")
+# print()
+
+# array = [1, 2, 3, 4, 5, 6]
+# for n in range (0, 3):
+#     print(array[n])
+# number = 4
+# if n == number:
+#     print(n)
+
+# arr = [2, 4, 6, 8, 10, 5, 12, 14, 16]
+# number = 8
+# for n in arr:
+#     print(n)
+#     if n == number:
+#         print(n)
+
+# number = 6
+# arr = [2, 4, 6, 8, 10, 5, 12, 14, 16]
+# for n in range (len(arr)):
+#     print(arr[n])
+#     if arr[n] == number:
+#         print('found this position', n)
+
+# number = 10
+# arr = [2, 4, 6, 8, 10, 5, 12, 14, 16]
+# for n in arr:
+#     print(arr[n])
+#     if arr[n] == number:
+#         print('found this position', n)
+
+
+
+
+
+
+
+
+
+
